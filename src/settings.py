f28976bb4f0007a7e77638d42fb8c9903e061d7a
Varitius24 <149641921+Varitius24@users.noreply.github.com>
2024-02-24 15:41:24 +0000

    added file to set the bands that will be used

---
+++ src/settings.py
@@ -0,0 +1,14 @@
+
+"""Works for this dataset https://docs.google.com/document/d/1vR_zxttAy94d5cXQUAD_iZoFruaWzda6QsRkg_LUwDo/edit?usp=sharing
+        Change with the appropiate bands for your data"""
+        
+RED_BAND = 14
+GREEN_BAND = 7
+BLUE_BAND = 2
+RED_EDGE_BAND = 15
+NIR_BAND = 21
+
+"A range of bands can be specified as follows but it is not advisable as it adds unnecesary noise to the image"
+# RED_EDGE_BAND = (15,18)
+# NIR_BAND = (20,32)
+
